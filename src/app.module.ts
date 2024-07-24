30b465d978d6a7aca70b10acdf91b6587e5e2c21
Miguel Abache <miiguel.abache@gmail.com>
2024-07-24 11:38:22 -0400

    Config env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as morgan from 'morgan';
 import { AppModule } from './app.module';
+import { CORS } from './constants/cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configServices = app.get(ConfigService);
+
+  app.use(morgan('dev'));
+
+  app.setGlobalPrefix('v1');
+
+  app.enableCors(CORS);
+  await app.listen(configServices.get('PORT'));
+  console.log(`App running on : ${await app.getUrl()}`);
 }
 bootstrap();
