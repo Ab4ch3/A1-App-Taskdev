9ca9a4484fef0c2c510ce0c1cb3942bcf4b009cb
Miguel Abache <miiguel.abache@gmail.com>
2024-08-01 12:37:25 -0400

    Projects services

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ProjectsService } from './projects.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProjectsEntity } from './entities/project.entity';
 import { ProjectsController } from './projects.controller';
+import { ProjectsService } from './projects.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ProjectsEntity])],
   controllers: [ProjectsController],
   providers: [ProjectsService],
 })
--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,26 +1,98 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ErrorManager } from 'src/utils/error.manager';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
+import { ProjectsEntity } from './entities/project.entity';
 
 @Injectable()
 export class ProjectsService {
-  create(createProjectDto: CreateProjectDto) {
-    return 'This action adds a new project';
+  constructor(
+    @InjectRepository(ProjectsEntity)
+    private readonly projectRepository: Repository<ProjectsEntity>,
+  ) {}
+  async create(newProject: CreateProjectDto): Promise<ProjectsEntity> {
+    try {
+      return await this.projectRepository.save(newProject);
+    } catch (e) {
+      throw ErrorManager.createSignatureError(e.message);
+    }
   }
 
-  findAll() {
-    return `This action returns all projects`;
+  async findAll(): Promise<ProjectsEntity[]> {
+    try {
+      const projects: ProjectsEntity[] = await this.projectRepository.find();
+
+      if (projects.length === 0) {
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: 'Not Found projects',
+        });
+      }
+
+      return projects;
+    } catch (e) {
+      throw ErrorManager.createSignatureError(e.message);
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} project`;
+  async findProject(id: string): Promise<ProjectsEntity> {
+    try {
+      const project: ProjectsEntity = await this.projectRepository
+        .createQueryBuilder('project')
+        .where({ id })
+        .getOne();
+
+      if (!project) {
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: 'Not Found project',
+        });
+      }
+      return project;
+    } catch (e) {
+      throw ErrorManager.createSignatureError(e.message);
+    }
   }
 
-  update(id: number, updateProjectDto: UpdateProjectDto) {
-    return `This action updates a #${id} project`;
+  async update(
+    id: string,
+    updateProject: UpdateProjectDto,
+  ): Promise<UpdateResult | undefined> {
+    try {
+      const project: UpdateResult = await this.projectRepository.update(
+        id,
+        updateProject,
+      );
+
+      if (project.affected === 0) {
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: 'Not Updated project',
+        });
+      }
+
+      return project;
+    } catch (e) {
+      throw ErrorManager.createSignatureError(e.message);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} project`;
+  async remove(id: string): Promise<DeleteResult | undefined> {
+    try {
+      const project: DeleteResult = await this.projectRepository.delete(id);
+
+      if (project.affected === 0) {
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: 'Not Delete project',
+        });
+      }
+
+      return project;
+    } catch (e) {
+      throw ErrorManager.createSignatureError(e.message);
+    }
   }
 }
